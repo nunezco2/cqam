4ba0352768cb6659440a52e6b56ccf236df0537d
Santiago Nunez-Corrales <nunezco2@illinois.edu>
2025-03-18 13:16:42 -0400

    Add program state word

--- cqam-vm/src/context.rs
+++ cqam-vm/src/context.rs
@@ -3,12 +3,16 @@
 use cqam_core::memory::{CMEM, QMEM};
 use cqam_core::register::RegisterBank;
 use crate::resource::ResourceTracker;
+use crate::psw::ProgramStateWord;
+use crate::simconfig::QuantumFidelityThreshold;
 
 pub struct ExecutionContext {
     pub pc: usize,
     pub cmem: CMEM,
     pub qmem: QMEM<i32>,
     pub registers: RegisterBank,
+    pub psw: ProgramStateWord,
+    pub config: QuantumFidelityThreshold,
     pub program: Vec<String>,
     pub resource_tracker: ResourceTracker,
 }
@@ -20,6 +24,8 @@ impl ExecutionContext {
             cmem: CMEM::new(),
             qmem: QMEM::new(),
             registers: RegisterBank::new(),
+            psw: ProgramStateWord::new(),
+            config: QuantumFidelityThreshold::default(),
             program,
             resource_tracker: ResourceTracker::new(),
         }
